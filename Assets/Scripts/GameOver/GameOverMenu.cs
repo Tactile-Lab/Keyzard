147bf137afbbaca5493356d70a08ad061d2266dd
AjaxDh <ajax.dh2@gmail.com>
2026-03-25 05:01:55 +0100

    fix : main menu now reset inventory

--- Assets/Scripts/GameOver/GameOverMenu.cs
+++ Assets/Scripts/GameOver/GameOverMenu.cs
@@ -67,7 +67,8 @@ public class GameOverMenu : MonoBehaviour
     }
 
     void RetourMenu()
-    {
+    {   
+        SpellInventoryManager.Instance.ResetInventory();
         SceneManager.LoadScene(0);
     }
 }
